a69ae0ba90015ef238fbf5c1747c8fd6fb751baa
XtratoS <mamdouh.waleed.96@gmail.com>
2019-12-26 05:25:46 +0200

    Added helper functions for password hashing, checking and rendering errors

---
+++ helpers.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def gen_hash(pw):
+    pwhash = generate_password_hash(pw, method=f'pbkdf2:sha256:150000', salt_length=8)
+    print(pwhash)
+    return pwhash
+
+
+def recollect_hash(pw, salt):
+    return f"pbkdf2:sha256:150000${salt}${pw}"
+
+
+def error(msg):
+    return render_template("error.html", msg=msg)
